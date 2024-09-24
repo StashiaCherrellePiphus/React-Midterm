0e29670e43889b4f3e4ee7aa9c0481a32894397d
Stashia Piphus <spiphus.prsvr@gmail.com>
2024-09-24 10:02:32 -0500

    update

--- midterm/src/index.js
+++ midterm/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 import Layout from './Pages/Layout';
 import About from './Pages/About';
 import Home from './Pages/Home';
 import Contact from './Pages/Contact';
 import Creator from './Pages/Creator';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
-
+import '../Pages/Navbar.js';
+import './App.css';
 
 
  function App() {
@@ -18,6 +20,7 @@ import { BrowserRouter, Routes, Route} from 'react-router-dom';
        <Route path="about" element={<About />} />
        <Route path="contact" element={<Contact />} />
        <Route path="creator" element={<Creator />} />
+       <Route path="navbar.js" element={<Navbar.js />} />
       </Route>
     </Routes>
     </BrowserRouter>
